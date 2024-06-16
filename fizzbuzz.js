8cfc6055c0a33aca003a193e6b1e66ac9a25428e
OnelKavindhu <106512102+OnelKavindhu@users.noreply.github.com>
2024-06-16 19:07:24 +0530

    test commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,16 @@
-//Divisable by 3 => Fizz
-//Divisable by 6 => Buzz
-//Divisable by 3 & 6 => FizzBuzz
-//Not divisable by 3 & 6 => input
-//Input is not a number => input 
-
-
-const output = fizzBuzz()
-console.log(output)
-
-
+const output = fizzBuzz(5)
 
 function fizzBuzz(input){
     if(typeof input !== 'numer')
-        console.log("Not a Number")
-    elif(input %3 == 0)
+        console.log("Not a Number"); 
+    if(input %3 === 0)
         console.log("Fizz")
-    elif(input %6 == 0)
+    if(input %5 === 0)
         console.log("Buzz")
+    if(input %3 === 0 && input %5 === 0)
+        console.log("FizzBuzz")
+    if(input %3 !== 0 || input %5 !== 0)
+        console.log(input)
+
+    
 }
